44a2a6babc2305ddf8c71ba5323a3624206a2b34
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-04-18 17:34:09 -0700

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+import DisplayData from "./DisplayData";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-
+function Form({ handleFirstNameChange, handleLastNameChange, firstName, lastName }) {
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
